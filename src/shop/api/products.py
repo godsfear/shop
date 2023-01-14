dc4337c6c2637066f0736eff4e04797e6787311c
godsfear <44734149+godsfear@users.noreply.github.com>
2023-01-14 21:23:33 +0600

    async

--- src/shop/api/products.py
+++ src/shop/api/products.py
@@ -13,26 +13,26 @@ router = APIRouter(
 
 
 @router.get('/', response_model=List[Products])
-def get_products(service: ProductsService = Depends()):
+async def get_products(service: ProductsService = Depends()):
     return service.get_all()
 
 
 @router.get('/{product_id}', response_model=Products)
-def get_product_by_id(product_id: int, service: ProductsService = Depends()):
+async def get_product_by_id(product_id: int, service: ProductsService = Depends()):
     return service.get_by_id(product_id)
 
 
 @router.post('/', response_model=Products)
-def create_products(products_data: ProductsCreate, service: ProductsService = Depends()):
+async def create_products(products_data: ProductsCreate, service: ProductsService = Depends()):
     return service.create(products_data)
 
 
 @router.put('/{product_id}', response_model=Products)
-def update_product(product_id: int, product_data: ProductsUpdate, service: ProductsService = Depends()):
+async def update_product(product_id: int, product_data: ProductsUpdate, service: ProductsService = Depends()):
     return service.update(product_id, product_data)
 
 
 @router.delete('/{product_id}')
-def delete_product(product_id: int, service: ProductsService = Depends()):
+async def delete_product(product_id: int, service: ProductsService = Depends()):
     service.delete(product_id)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
