7d3f488d377084b0687e4e69b5730ae84005849b
godsfear <44734149+godsfear@users.noreply.github.com>
2023-02-27 05:47:26 +0600

    no comments

--- src/shop/api/__init__.py
+++ src/shop/api/__init__.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 from .entity import router as entity_router
 from .user import router as user_router
 from .auth import router as auth_router
+from .person import router as person_router
 
 router = APIRouter()
 router.include_router(auth_router)
 router.include_router(entity_router)
 router.include_router(user_router)
+router.include_router(person_router)
---
+++ src/shop/api/person.py
@@ -0,0 +1,40 @@
+import uuid
+from fastapi import APIRouter, Depends
+from typing import List
+
+from ..models.person import Person, PersonCreate, PersonUpdate, PersonBase
+from ..services.person import PersonService
+
+router = APIRouter(
+    prefix='/person',
+)
+
+
+@router.get('/all', response_model=List[Person])
+async def get_person(service: PersonService = Depends()):
+    person = await service.get_all()
+    return person
+
+
+@router.get('/{person_id}', response_model=Person)
+async def get_person_by_id(person_id: uuid.UUID, service: PersonService = Depends()):
+    person = await service.get_by_id(person_id)
+    return person
+
+
+@router.post('/', response_model=Person)
+async def create_person(person_data: PersonCreate, service: PersonService = Depends()):
+    person = await service.create(person_data)
+    return person
+
+
+@router.put('/{person_id}', response_model=Person)
+async def update_person(person_id: uuid.UUID, person_data: PersonUpdate, service: PersonService = Depends()):
+    person = await service.update(person_id, person_data)
+    return person
+
+
+@router.delete('/{person_id}', response_model=Person)
+async def delete_person(person_id: uuid.UUID, service: PersonService = Depends()):
+    person = await service.expire(person_id)
+    return person
