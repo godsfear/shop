63fcd1c5efa6a4151ef732e0ffa42a6899fe71e3
godsfear <44734149+godsfear@users.noreply.github.com>
2023-03-11 01:10:08 +0600

    no comments

--- src/shop/models/user.py
+++ src/shop/models/user.py
@@ -4,22 +4,30 @@ from pydantic import BaseModel
 
 
 class UserBase(BaseModel):
-    username: str
+    email: str | None
+    phone: str | None
     person_id: uuid.UUID
+    checked: bool
     begins: datetime.datetime
     ends: datetime.datetime | None
+
+
+class UserEditable(BaseModel):
+    email: str | None
+    phone: str | None
 
 
 class UserCreate(UserBase):
     password: str
 
 
-class UserUpdate(UserBase):
+class UserUpdate(UserEditable):
     password: str
 
 
-class UserSave(UserBase):
+class UserSave(UserEditable):
     passhash: str
+    checked: bool
 
 
 class User(UserBase):
