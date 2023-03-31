2634561016045b5fd4b6ed97a658dd6c2278a4ed
godsfear <44734149+godsfear@users.noreply.github.com>
2023-03-31 22:01:11 +0600

    no comments

--- src/shop/api/__init__.py
+++ src/shop/api/__init__.py
@@ -4,7 +4,12 @@ from .user import router as user_router
 from .auth import router as auth_router
 from .person import router as person_router
 from .property import router as property_router
-from .property import router as category_router
+from .category import router as category_router
+from .currency import router as currency_router
+from .company import router as company_router
+from .country import router as country_router
+from .place import router as place_router
+
 
 router = APIRouter(prefix='/api/v1')
 router.include_router(auth_router)
@@ -13,3 +18,7 @@ router.include_router(user_router)
 router.include_router(person_router)
 router.include_router(property_router)
 router.include_router(category_router)
+router.include_router(currency_router)
+router.include_router(company_router)
+router.include_router(country_router)
+router.include_router(place_router)
--- src/shop/models/currency.py
+++ src/shop/models/currency.py
@@ -6,23 +6,28 @@ from pydantic import BaseModel
 class CurrencyBase(BaseModel):
     category: uuid.UUID
     code: str
+    numcode: int
+
+
+class CurrencyUpdate(CurrencyBase):
     name: str
+    name_plural: str
+    symbol: str
+    symbol_native: str
+    decimal_digits: int
+    rounding: float
     description: str | None
     begins: datetime.datetime
     ends: datetime.datetime | None
     author: uuid.UUID
 
 
-class Currency(CurrencyBase):
+class Currency(CurrencyUpdate):
     id: uuid.UUID
 
     class Config:
         orm_mode = True
 
 
-class CurrencyCreate(CurrencyBase):
-    pass
-
-
-class CurrencyUpdate(CurrencyBase):
+class CurrencyCreate(CurrencyUpdate):
     pass
