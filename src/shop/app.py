d45dff73264df41f4132739029fbc126449e736b
godsfear <dmitriy.degtyaryov@gmail.com>
2024-08-07 13:54:57 +0500

    async

--- src/shop/app.py
+++ src/shop/app.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from starlette.middleware.base import BaseHTTPMiddleware
-from shop.middleware import middleware_proc
 
+from shop.middleware import middleware_proc
 from shop.api import router
 
+
 app = FastAPI()
-#app.add_middleware(BaseHTTPMiddleware, dispatch=middleware_proc)
+app.add_middleware(BaseHTTPMiddleware, dispatch=middleware_proc)
 app.include_router(router)
